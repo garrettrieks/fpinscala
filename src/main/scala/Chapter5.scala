286798ba87ee0ff4e3105174a4d38a180894351e
Garrett Rieks <garrett.rieks@banno.com>
2015-10-17 20:55:52 -0500

    Exercises 5.6 through 5.14

--- src/main/scala/Chapter5.scala
+++ src/main/scala/Chapter5.scala
@@ -65,12 +65,66 @@ trait Stream[+A] {
   def takeWhileF(p: A => Boolean): Stream[A] =
     foldRight(empty[A]: Stream[A])((a,b) => if (p(a)) cons(a,b) else empty)
 
-  def headOption: Option[A] = sys.error("todo")
+  // 5.6
+  def headOption: Option[A] =
+    foldRight(None: Option[A])((a,b) => Some(a))
 
   // 5.7 map, filter, append, flatmap using foldRight. Part of the exercise is
   // writing your own function signatures.
+  def map[B](f: A => B): Stream[B] =
+    foldRight(empty[B])((a,acc) => cons(f(a), acc))
 
-  def startsWith[B](s: Stream[B]): Boolean = sys.error("todo")
+  def filter(f: A => Boolean): Stream[A] =
+    foldRight(empty[A])((a,acc) => if (f(a)) cons(a, acc) else acc)
+
+  // come back to this
+  def append[B>:A](s: => Stream[B]): Stream[B] =
+    foldRight(s)((a,acc) => cons(a, acc))
+
+  def flatMap[B](f: A => Stream[B]): Stream[B] =
+    foldRight(empty[B])((a,acc) => f(a).append(acc))
+
+  // 5.13
+  def mapU[B](f: A => B): Stream[B] = {
+    unfold(this){
+      case Cons(h,t) => Some((f(h()), t()))
+      case _ => None
+    }
+  }
+
+  def takeU(n: Int): Stream[A] = {
+    unfold(this, n){
+      case (Cons(h,t), num) if num > 0 => Some((h(), (t(), num-1)))
+      case _ => None
+    }
+  }
+
+  def takeWhileU(p: A => Boolean): Stream[A] = {
+    unfold(this, p){
+      case (Cons(h,t), f) if f(h()) => Some((h(), (t(), f)))
+      case _ => None
+    }
+  }
+
+  def zipWith[B,C](s: Stream[B])(f: (A,B) => C): Stream[C] = {
+    unfold((this, s)){
+      case (Cons(h1,t1), Cons(h2,t2)) => Some((f(h1(),h2()), (t1(), t2())))
+      case _ => None
+    }
+  }
+
+  def zipAll[B](s2: Stream[B]): Stream[(Option[A],Option[B])] = {
+    unfold((this, s2)){
+      case (Cons(h1,t1), Cons(h2,t2)) => Some(((Some(h1()), Some(h2())), (t1(), t2())))
+      case (Cons(h1,t1), Empty) => Some(((Some(h1()), None), (t1(), Empty)))
+      case (Empty, Cons(h2,t2)) => Some(((None, Some(h2())), (Empty, t2())))
+      case _ => None
+    }
+  }
+
+  def startsWith[B](s: Stream[B]): Boolean = {
+    (this.zipWith(s)((a,b) => if (a == b) b).toList == s.toList)
+  }
 }
 case object Empty extends Stream[Nothing]
 case class Cons[+A](h: () => A, t: () => Stream[A]) extends Stream[A]
@@ -89,7 +143,41 @@ object Stream {
     else cons(as.head, apply(as.tail: _*))
 
   val ones: Stream[Int] = Stream.cons(1, ones)
-  def from(n: Int): Stream[Int] = sys.error("todo")
 
-  def unfold[A, S](z: S)(f: S => Option[(A, S)]): Stream[A] = sys.error("todo")
+  // 5.8
+  def constant[A](a: A): Stream[A] =
+    cons(a, constant(a))
+
+  // 5.9
+  def from(n: Int): Stream[Int] =
+    cons(n, from(n+1))
+
+  // 5.10
+  def fibs: Stream[Int] = {
+    def loop(n1: Int, n2: Int): Stream[Int] =
+      cons(n1, loop(n2, n1+n2))
+    loop(0,1)
+  }
+
+  // 5.11
+  def unfold[A, S](z: S)(f: S => Option[(A, S)]): Stream[A] = {
+    f(z) match {
+      case Some((a,s)) => cons(a,unfold(s)(f))
+      case None => empty
+    }
+  }
+
+  // 5.12
+  def fibsU: Stream[Int] =
+    unfold((0,1)){ case (n1, n2) => Some((n1, (n2, n1 + n2))) }
+
+  def fromU(i: Int): Stream[Int] =
+    unfold(i)(n => Some((n, n+1)))
+
+  def constantU[A](a: A): Stream[A] =
+    unfold(a)(aa => Some((a,a)))
+
+  def onesU: Stream[Int] =
+    unfold(1)(n => Some((n,n)))
+
 }
--- src/test/scala/Chapter5Spec.scala
+++ src/test/scala/Chapter5Spec.scala
@@ -80,6 +80,118 @@ object Chapter5Spec extends Specification {
     }
   }
 
+  "Exercise 5.6" should {
+    "return the head of a Stream" in new context {
+      intStream.headOption must beSome(1)
+    }
+
+    "return None for an Empty Stream" in new context {
+      Stream().headOption must beNone
+    }
+  }
+
+  "Exercise 5.7" should {
+    "map a Stream using foldRight" in new context {
+      intStream.map(_+1).toList must_== List(2,3,4)
+    }
+
+    "filter a Stream using foldRight" in new context {
+      intStream.filter(_ > 1).toList must_== List(2,3)
+    }
+
+    "append a Stream using foldRight" in new context {
+      intStream.append(Stream(4,5)).toList must_== List(1,2,3,4,5)
+    }
+
+    "flatMap a Stream using foldRight" in new context {
+      intStream.flatMap(a => Stream(a*a)).toList must_== List(1,4,9)
+    }
+  }
+
+  "Exercise 5.8" should {
+    "generate a stream of constant values" in new context {
+      constant(0).take(5).toList must_== List(0,0,0,0,0)
+    }
+  }
+
+  "Exercise 5.9" should {
+    "generate a stream of integers from the starting integer" in new context {
+      from(1).take(5).toList must_== List(1,2,3,4,5)
+    }
+  }
+
+  "Exercise 5.10" should {
+    "generate a stream of fibonacci numbers" in new context {
+      fibs.take(7).toList must_== List(0,1,1,2,3,5,8)
+    }
+  }
+
+  "Exercise 5.12" should {
+    "generate a stream of fibonacci numbers using unfold" in new context {
+      fibsU.take(7).toList must_== List(0,1,1,2,3,5,8)
+    }
+
+    "generate a stream of integers from the starting integer using unfold" in new context {
+      fromU(1).take(5).toList must_== List(1,2,3,4,5)
+    }
+
+    "generate a stream of constant values using unfold" in new context {
+      constantU(0).take(5).toList must_== List(0,0,0,0,0)
+    }
+
+    "generate a stream of ones using unfold" in new context {
+      onesU.take(5).toList must_== List(1,1,1,1,1)
+    }
+  }
+
+  "Exercise 5.13" should {
+    "map a Stream using unfold" in new context {
+      intStream.mapU(_+1).toList must_== List(2,3,4)
+    }
+
+    "take n elements from a Stream using unfold" in new context {
+      intStream.takeU(2).toList must_== List(1,2)//Cons(() => 1, Cons(() => 2, () => Empty))
+    }
+
+    "take 0 elements from a stream using unfold" in new context {
+      intStream.takeU(0) must_== Empty
+    }
+
+    "take the first elements that meet predicate using unfold" in new context {
+      Stream(1,3,2).takeWhileU(_ < 3).toList must_== List(1)
+    }
+
+    "don't take any if predicate is not met using unfold" in new context {
+      intStream.takeWhileU(_ < 1) must_== Empty
+    }
+
+    "zipWith a Stream using unfold" in new context {
+      intStream.zipWith(intStream)((a,b) => a+b).toList must_== List(2,4,6)
+    }
+
+    "zipAll a Stream with less elements using unfold" in new context {
+      intStream.zipAll(Stream(1,2)).toList must_== List((Some(1), Some(1)), (Some(2), Some(2)), (Some(3), None))
+    }
+
+    "zipAll a Stream with more elements using unfold" in new context {
+      intStream.zipAll(Stream(1,2,3,4)).toList must_== List((Some(1), Some(1)), (Some(2), Some(2)), (Some(3), Some(3)), (None, Some(4)))
+    }
+
+    "zipAll Streams with no elements using unfold" in new context {
+      Stream().zipAll(Stream()) must_== Empty
+    }
+  }
+
+  "Exercise 5.14" should {
+    "return True when one stream startsWith the other" in new context {
+      intStream.startsWith(Stream(1,2)) must beTrue
+    }
+
+    "return False when one stream doesn't startWith the other" in new context {
+      intStream.startsWith(Stream(2)) must beFalse
+    }
+  }
+
   trait context extends Scope {
     lazy val intStream = Stream(1,2,3)
   }
